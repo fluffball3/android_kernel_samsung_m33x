efbae48e3aa84ae7d771a4560ff9aeb0ee3ab88e
David Hildenbrand <david@redhat.com>
2021-04-22 12:08:20 +0200

    mm: remove VM_DENYWRITE
    All in-tree users of MAP_DENYWRITE are gone. MAP_DENYWRITE cannot be
    set from user space, so all users are gone; let's remove it.
    
    Acked-by: "Eric W. Biederman" <ebiederm@xmission.com>
    Acked-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: David Hildenbrand <david@redhat.com>
    [fluffball3: backport to android 5.10 lts which contains several android specific and already merged upstream patches]
    Signed-off-by: Shon Sebastian <fluffball3@proton.me>

--- include/linux/mman.h
+++ include/linux/mman.h
@@ -156,7 +156,6 @@ static inline unsigned long
 calc_vm_flag_bits(struct file *file, unsigned long flags)
 {
 	return _calc_vm_trans(flags, MAP_GROWSDOWN,  VM_GROWSDOWN ) |
-	       _calc_vm_trans(flags, MAP_DENYWRITE,  VM_DENYWRITE ) |
 	       _calc_vm_trans(flags, MAP_LOCKED,     VM_LOCKED    ) |
 	       _calc_vm_trans(flags, MAP_SYNC,	     VM_SYNC      ) |
 	       arch_calc_vm_flag_bits(file, flags);
