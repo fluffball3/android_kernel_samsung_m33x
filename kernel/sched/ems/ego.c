6a69d8914e611974eefbe96c178999c5513dcb81
Lukasz Luba <lukasz.luba@arm.com>
2021-06-14 20:12:38 +0100

    sched/ems: ego: Consider reduced CPU capacity in energy calculation
    Energy Aware Scheduling (EAS) needs to predict the decisions made by
    SchedUtil. The map_util_freq() exists to do that.
    
    There are corner cases where the max allowed frequency might be reduced
    (due to thermal). SchedUtil as a CPUFreq governor, is aware of that
    but EAS is not. This patch aims to address it.
    
    SchedUtil stores the maximum allowed frequency in
    'sugov_policy::next_freq' field. EAS has to predict that value, which is
    the real used frequency. That value is made after a call to
    cpufreq_driver_resolve_freq() which clamps to the CPUFreq policy limits.
    In the existing code EAS is not able to predict that real frequency.
    This leads to energy estimation errors.
    
    To avoid wrong energy estimation in EAS (due to frequency miss prediction)
    make sure that the step which calculates Performance Domain frequency,
    is also aware of the allowed CPU capacity.
    
    Furthermore, modify map_util_freq() to not extend the frequency value.
    Instead, use map_util_perf() to extend the util value in both places:
    SchedUtil and EAS, but for EAS clamp it to max allowed CPU capacity.
    In the end, we achieve the same desirable behavior for both subsystems
    and alignment in regards to the real CPU frequency.
    
    Signed-off-by: Lukasz Luba <lukasz.luba@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Acked-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com> (For the schedutil part)
    Link: https://lore.kernel.org/r/20210614191238.23224-1-lukasz.luba@arm.com
    [Sultan Alsawaf: backport to 5.10]
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    [fluffball3: added to energyaware scheduler]
    Signed-off-by: Shon Sebastian <fluffball3@proton.me>

--- kernel/sched/ems/ego.c
+++ kernel/sched/ems/ego.c
@@ -556,10 +556,15 @@ static void ego_deferred_update(struct ego_policy *egp, u64 time,
 }
 
 static inline unsigned long
-ego_map_util_freq(struct ego_policy *egp, unsigned long util,
+ego_map_util_freq(unsigned long util,
 		  unsigned long freq, unsigned long cap)
 {
-	return ((freq * (100 + egp->pelt_margin)) / 100) * util / cap;
+	return freq * util / cap;
+}
+
+static inline unsigned long ego_map_util_perf(struct ego_policy *egp, unsigned long util)
+{
+	return ((util * (100 + egp->pelt_margin)) / 100);
 }
 
 /**
@@ -607,7 +612,8 @@ static unsigned int get_next_freq(struct ego_policy *egp,
 	unsigned int freq, org_freq, eng_freq = 0;
 
 	/* compute pure frequency base on util */
-	org_freq = ego_map_util_freq(egp, util, policy->cpuinfo.max_freq, max);
+	util = ego_map_util_perf(egp, util);
+	org_freq = ego_map_util_freq(util, policy->cpuinfo.max_freq, max);
 	if ((org_freq == egp->cached_raw_freq || egp->work_in_progress)
 					&& !egp->need_freq_update) {
 		freq = max(egp->org_freq, egp->next_freq);
