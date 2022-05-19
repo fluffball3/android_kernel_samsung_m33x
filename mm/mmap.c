2a42a4541c3cb0f6563b28bbfb1653c43e85108c
Yang Shi <shy828301@gmail.com>
2022-05-19 14:08:50 -0700

    mm: khugepaged: introduce khugepaged_enter_vma() helper
    The khugepaged_enter_vma_merge() actually does as the same thing as the
    khugepaged_enter() section called by shmem_mmap(), so consolidate them
    into one helper and rename it to khugepaged_enter_vma().
    
    Link: https://lkml.kernel.org/r/20220510203222.24246-8-shy828301@gmail.com
    Signed-off-by: Yang Shi <shy828301@gmail.com>
    Acked-by: Vlastmil Babka <vbabka@suse.cz>
    Cc: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Cc: Matthew Wilcox (Oracle) <willy@infradead.org>
    Cc: Miaohe Lin <linmiaohe@huawei.com>
    Cc: Rik van Riel <riel@surriel.com>
    Cc: Song Liu <song@kernel.org>
    Cc: Song Liu <songliubraving@fb.com>
    Cc: Theodore Ts'o <tytso@mit.edu>
    Cc: Zi Yan <ziy@nvidia.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/mmap.c
+++ mm/mmap.c
@@ -1285,7 +1285,7 @@ struct vm_area_struct *__vma_merge(struct mm_struct *mm,
 					   keep_locked);
 		if (err)
 			return NULL;
-		khugepaged_enter_vma_merge(prev, vm_flags);
+		khugepaged_enter_vma(prev, vm_flags);
 		return prev;
 	}
 
@@ -1315,7 +1315,7 @@ struct vm_area_struct *__vma_merge(struct mm_struct *mm,
 		}
 		if (err)
 			return NULL;
-		khugepaged_enter_vma_merge(area, vm_flags);
+		khugepaged_enter_vma(area, vm_flags);
 		return area;
 	}
 
@@ -2599,7 +2599,7 @@ int expand_upwards(struct vm_area_struct *vma, unsigned long address)
 		}
 	}
 	anon_vma_unlock_write(vma->anon_vma);
-	khugepaged_enter_vma_merge(vma, vma->vm_flags);
+	khugepaged_enter_vma(vma, vma->vm_flags);
 	validate_mm(mm);
 	return error;
 }
@@ -2677,7 +2677,7 @@ int expand_downwards(struct vm_area_struct *vma,
 		}
 	}
 	anon_vma_unlock_write(vma->anon_vma);
-	khugepaged_enter_vma_merge(vma, vma->vm_flags);
+	khugepaged_enter_vma(vma, vma->vm_flags);
 	validate_mm(mm);
 	return error;
 }
