c8abce3329f7dac9a4efe8d7f3f6626a855dbcf8
Dietmar Eggemann <dietmar.eggemann@arm.com>
2023-05-15 13:57:35 +0200

    sched/fair, cpufreq: Introduce 'runnable boosting'
    The responsiveness of the Per Entity Load Tracking (PELT) util_avg in
    mobile devices is still considered too low for utilization changes
    during task ramp-up.
    
    In Android this manifests in the fact that the first frames of a UI
    activity are very prone to be jankframes (a frame which doesn't meet
    the required frame rendering time, e.g. 16ms@60Hz) since the CPU
    frequency is normally low at this point and has to ramp up quickly.
    
    The beginning of an UI activity is also characterized by the occurrence
    of CPU contention, especially on little CPUs. Current little CPUs can
    have an original CPU capacity of only ~ 150 which means that the actual
    CPU capacity at lower frequency can even be much smaller.
    
    Schedutil maps CPU util_avg into CPU frequency request via:
    
      util = effective_cpu_util(..., cpu_util_cfs(cpu), ...) ->
      util = map_util_perf(util) -> freq = map_util_freq(util, ...)
    
    CPU contention for CFS tasks can be detected by 'CPU runnable > CPU
    utililization' in cpu_util_cfs_boost() -> cpu_util(..., boost = 1).
    Schedutil uses 'runnable boosting' by calling cpu_util_cfs_boost().
    
    To be in sync with schedutil's CPU frequency selection, Energy Aware
    Scheduling (EAS) also calls cpu_util(..., boost = 1) during max util
    detection.
    
    Moreover, 'runnable boosting' is also used in load-balance for busiest
    CPU selection when the migration type is 'migrate_util', i.e. only at
    sched domains which don't have the SD_SHARE_PKG_RESOURCES flag set.
    
    Suggested-by: Vincent Guittot <vincent.guittot@linaro.org>
    Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Link: https://lore.kernel.org/r/20230515115735.296329-3-dietmar.eggemann@arm.com
    [fluffball3: backport to 5.10]
    Signed-off-by: fluffball3 <fluffball3@proton.me>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -2459,6 +2459,7 @@ static inline void cpufreq_update_util(struct rq *rq, unsigned int flags) {}
 #endif /* CONFIG_CPU_FREQ */
 
 extern unsigned long cpu_util_cfs(int cpu);
+extern unsigned long cpu_util_cfs_boost(int cpu);
 
 #ifdef CONFIG_UCLAMP_TASK
 unsigned long uclamp_eff_value(struct task_struct *p, enum uclamp_id clamp_id);
@@ -2654,6 +2655,9 @@ static inline unsigned long cpu_util_dl(struct rq *rq)
 static inline unsigned long cpu_util_schedutil(struct rq *rq)
 {
 	unsigned long util = READ_ONCE(rq->cfs.avg.util_avg);
+	unsigned long runnable = READ_ONCE(rq->cfs.avg.runnable_avg);
+
+	util = max(util, runnable);
 
 	if (sched_feat(UTIL_EST)) {
 		util = max_t(unsigned long, util,
