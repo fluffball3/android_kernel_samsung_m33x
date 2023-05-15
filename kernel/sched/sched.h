f3f903d160acbced20c4ce7752b7f65d4bad2f1c
Dietmar Eggemann <dietmar.eggemann@arm.com>
2023-05-15 13:57:34 +0200

    sched/fair: Refactor CPU utilization functions
    There is a lot of code duplication in cpu_util_next() & cpu_util_cfs().
    
    Remove this by allowing cpu_util_next() to be called with p = NULL.
    Rename cpu_util_next() to cpu_util() since the '_next' suffix is no
    longer necessary to distinct cpu utilization related functions.
    Implement cpu_util_cfs(cpu) as cpu_util(cpu, p = NULL, -1).
    
    This will allow to code future related cpu util changes only in one
    place, namely in cpu_util().
    
    Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Link: https://lore.kernel.org/r/20230515115735.296329-2-dietmar.eggemann@arm.com
    [fluffball3: backport to 5.10]
    Signed-off-by: fluffball3 <fluffball3@proton.me>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -2458,6 +2458,8 @@ static inline void cpufreq_update_util(struct rq *rq, unsigned int flags)
 static inline void cpufreq_update_util(struct rq *rq, unsigned int flags) {}
 #endif /* CONFIG_CPU_FREQ */
 
+extern unsigned long cpu_util_cfs(int cpu);
+
 #ifdef CONFIG_UCLAMP_TASK
 unsigned long uclamp_eff_value(struct task_struct *p, enum uclamp_id clamp_id);
 
