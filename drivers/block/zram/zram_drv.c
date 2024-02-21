0097bd7fb970ac3859e85b856f3ac2fa6475ced5
Ruchit <risenid@duck.com>
2024-02-21 21:17:01 +0530

    zram: Protect handle_decomp_fail behind a check
    the previous definitions as well as the creation of this is locked behind CONFIG_ZRAM_LRU_WRITEBACK as well
    
    Change-Id: I869b5595f69cc481e93ca6862b460594762d9b25
    Signed-off-by: Ruchit <risenid@duck.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -2967,10 +2967,12 @@ static int __zram_bvec_read(struct zram *zram, struct page *page, u32 index,
 		zcomp_stream_put(zram->comp);
 	}
 
+#ifdef CONFIG_ZRAM_LRU_WRITEBACK
 	/* Should NEVER happen. BUG() if it does. */
 	if (unlikely(ret))
 		handle_decomp_fail(zram->compressor, ret, index, src, size,
 				   NULL);
+#endif
 
 	zs_unmap_object(zram->mem_pool, handle);
 #ifdef CONFIG_ZRAM_LRU_WRITEBACK
