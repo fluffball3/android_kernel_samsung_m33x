5070f7f7b61775d4276fc1c4013a19c811d5ea82
Sooyong Suk <s.suk@samsung.corp-partner.google.com>
2024-07-18 11:15:27 +0900

    ANDROID: mm: add vendor hook in fault and read file
    Add a vendor hook to notify vendor module fault and read events.
    
    Bug: 351175506
    Bug: 384865438
    Change-Id: I4c46e9e00aa5f5555fd42a6b0815563497658b34
    Signed-off-by: Sooyong Suk <s.suk@samsung.corp-partner.google.com>
    (cherry picked from commit a9867d872e24fbe658e05f32b770e4b36c6e3773)
    Signed-off-by: Rui Chen <chenrui9@honor.com>

--- mm/filemap.c
+++ mm/filemap.c
@@ -2232,6 +2232,7 @@ ssize_t generic_file_buffered_read(struct kiocb *iocb,
 		return 0;
 
 	iov_iter_truncate(iter, inode->i_sb->s_maxbytes);
+	trace_android_vh_filemap_read(filp, iocb->ki_pos, iov_iter_count(iter));
 
 	index = *ppos >> PAGE_SHIFT;
 	prev_index = ra->prev_pos >> PAGE_SHIFT;
@@ -3101,6 +3102,7 @@ vm_fault_t filemap_map_pages(struct vm_fault *vmf,
 	unsigned int mmap_miss = READ_ONCE(file->f_ra.mmap_miss);
 	vm_fault_t ret = (vmf->flags & FAULT_FLAG_SPECULATIVE) ?
 		VM_FAULT_RETRY : 0;
+	pgoff_t first_pgoff = 0;
 
 #ifdef CONFIG_PAGE_BOOST_RECORDING
 	pgoff_t head_pgoff = 0;
@@ -3110,6 +3112,7 @@ vm_fault_t filemap_map_pages(struct vm_fault *vmf,
 	head = first_map_page(mapping, &xas, end_pgoff);
 	if (!head)
 		goto out;
+	first_pgoff = xas.xa_index;
 
 #ifdef CONFIG_PAGE_BOOST_RECORDING
 	head_pgoff = xas.xa_index;
@@ -3165,6 +3168,7 @@ out:
 		record_io_info(file, head_pgoff, last_pgoff - head_pgoff + 1);
 #endif
 	WRITE_ONCE(file->f_ra.mmap_miss, mmap_miss);
+	trace_android_vh_filemap_map_pages(file, first_pgoff, last_pgoff, ret);
 	return ret;
 }
 EXPORT_SYMBOL(filemap_map_pages);
