b5e9086519af3a99e9cbb3d89cf0384dd1c86870
fluffball3 <132234302+fluffball3@users.noreply.github.com>
2024-09-23 23:45:03 +0530

    Revert "rpmsg: Fix calling device_lock() on non-initialized device"
    This reverts commit aac05aba509612ff561441be63ec84c9c1d4df6b.

--- include/linux/rpmsg.h
+++ include/linux/rpmsg.h
@@ -122,8 +122,6 @@ struct rpmsg_driver {
 
 #if IS_ENABLED(CONFIG_RPMSG)
 
-int rpmsg_register_device_override(struct rpmsg_device *rpdev,
-				   const char *driver_override);
 int register_rpmsg_device(struct rpmsg_device *dev);
 void unregister_rpmsg_device(struct rpmsg_device *dev);
 int __register_rpmsg_driver(struct rpmsg_driver *drv, struct module *owner);
@@ -150,12 +148,6 @@ int rpmsg_get_signals(struct rpmsg_endpoint *ept);
 int rpmsg_set_signals(struct rpmsg_endpoint *ept, u32 set, u32 clear);
 
 #else
-
-static inline int rpmsg_register_device_override(struct rpmsg_device *rpdev,
-						 const char *driver_override)
-{
-	return -ENXIO;
-}
 
 static inline int register_rpmsg_device(struct rpmsg_device *dev)
 {
