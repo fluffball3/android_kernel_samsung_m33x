1b6be6d0dc5012cd08508bbafd51e7545f8f70e4
fluffball3 <132234302+fluffball3@users.noreply.github.com>
2024-09-26 02:12:19 +0530

    make selinux easier to switch

--- security/selinux/selinuxfs.c
+++ security/selinux/selinuxfs.c
@@ -159,7 +159,11 @@ static ssize_t sel_write_enforce(struct file *file, const char __user *buf,
 	if (sscanf(page, "%d", &new_value) != 1)
 		goto out;
 
+/*permissive*/
 	new_value = 0;
+/*enforcing
+	new_value = !!new_value;*/
+
 
 	old_value = enforcing_enabled(state);
 	if (new_value != old_value) {
