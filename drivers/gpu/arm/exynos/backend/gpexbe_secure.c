1865b88986d828fc5de86d35647d169d73fa5930
fluffball3 <132234302+fluffball3@users.noreply.github.com>
2024-10-18 04:52:08 +0530

    update mali driver to r52p0

--- drivers/gpu/arm/exynos/backend/gpexbe_secure.c
+++ drivers/gpu/arm/exynos/backend/gpexbe_secure.c
@@ -21,9 +21,9 @@
 /* Implements */
 #include <gpexbe_secure.h>
 
-#ifdef CONFIG_MALI_DDK_VALHALL_R51P0
-#include "../../bv_r51p0/mali_kbase_hwaccess_pm.h"
-#include <linux/mali_drivers/bv_r51p0/protected_mode_switcher.h>
+#ifdef CONFIG_MALI_DDK_VALHALL_R52P0
+#include "../../bv_r52p0/mali_kbase_hwaccess_pm.h"
+#include <linux/mali_drivers/bv_r52p0/protected_mode_switcher.h>
 #endif
 #ifdef CONFIG_MALI_DDK_VALHALL_R49P1
 #include "../../bv_r49p1/mali_kbase_hwaccess_pm.h"
@@ -33,7 +33,7 @@
 /* Uses */
 #include <mali_kbase.h>
 
-#ifndef CONFIG_MALI_DDK_VALHALL_R51P0
+#ifndef CONFIG_MALI_DDK_VALHALL_R52P0
 #ifndef CONFIG_MALI_DDK_VALHALL_R49P1
 #include <linux/protected_mode_switcher.h>
 #endif
