43e53f8fb13189bdeed268f664500fd13f7a15d1
fluffball3 <132234302+fluffball3@users.noreply.github.com>
2025-02-26 12:47:12 +0530

    Revert "arm64: allow kprobes on EL0 handlers"
    This reverts commit e91337e32bb3c0f273354b2f1afee9ce199074cc.

--- arch/arm64/kernel/traps.c
+++ arch/arm64/kernel/traps.c
@@ -843,7 +843,7 @@ static const struct sys64_hook cp15_64_hooks[] = {
 	{},
 };
 
-void do_el0_cp15(unsigned long esr, struct pt_regs *regs)
+void do_cp15instr(unsigned int esr, struct pt_regs *regs)
 {
 	const struct sys64_hook *hook, *hook_base;
 
@@ -881,9 +881,10 @@ void do_el0_cp15(unsigned long esr, struct pt_regs *regs)
 	 */
 	do_undefinstr(regs, esr);
 }
+NOKPROBE_SYMBOL(do_cp15instr);
 #endif
 
-void do_el0_sys(unsigned long esr, struct pt_regs *regs)
+void do_sysinstr(unsigned int esr, struct pt_regs *regs)
 {
 	const struct sys64_hook *hook;
 
@@ -900,6 +901,7 @@ void do_el0_sys(unsigned long esr, struct pt_regs *regs)
 	 */
 	do_undefinstr(regs, esr);
 }
+NOKPROBE_SYMBOL(do_sysinstr);
 
 static const char *esr_class_str[] = {
 	[0 ... ESR_ELx_EC_MAX]		= "UNRECOGNIZED EC",
