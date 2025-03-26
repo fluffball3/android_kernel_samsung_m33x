726f26294074da12c86613d84742914358dfcb95
fluffball3 <fluffball3@proton.me>
2025-03-26 22:53:13 +0530

    add simple kernel and module build scripts
    referenced from Gabriel2392's build script. this only makes kernel and modules which is useful for repetitive building.

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-export PATH=$(pwd)/toolchain/clang/host/linux-x86/clang-r416183b/bin:$PATH
-export PATH=$(pwd)/toolchain/build/build-tools/path/linux-x86:$(pwd)/toolchain/prebuilts/gas/linux-x86:$PATH
-BUILD_ARGS="LOCALVERSION=-Elite3XP-1.0 KBUILD_BUILD_USER=fluffyball21 KBUILD_BUILD_HOST=Inudesu"
+export PATH=$(pwd)/../clang-r416183b/bin:$PATH
+export PATH=$(pwd)/../build-tools/path/linux-x86:$PATH
+KERNEL_HOME="$(pwd)"
+BUILD_ARGS="LOCALVERSION=-Elite3XP-v3.0 KBUILD_BUILD_USER=fluffyball21 KBUILD_BUILD_HOST=Inudesu"
 
-make O=out $BUILD_ARGS PLATFORM_VERSION=12 ANDROID_MAJOR_VERSION=s LLVM=1 LLVM_IAS=1 ARCH=arm64 TARGET_SOC=s5e8825 CROSS_COMPILE=$(pwd)/toolchain/clang/host/linux-x86/clang-r416183b/bin/aarch64-linux-gnu- m33x_defconfig
-make O=out $BUILD_ARGS PLATFORM_VERSION=12 ANDROID_MAJOR_VERSION=s LLVM=1 LLVM_IAS=1 ARCH=arm64 TARGET_SOC=s5e8825 CROSS_COMPILE=$(pwd)/toolchain/clang/host/linux-x86/clang-r416183b/bin/aarch64-linux-gnu- -j$(nproc --all)
+if [ ! -d "$KERNEL_HOME/build-tools" ]; then
+echo "Please execute "build.sh" instead"
+fi
+if [ ! -d "$KERNEL_HOME/clang-r416183b" ]; then
+    if [ -d "$KERNEL_HOME/build-tools" ]; then
+    echo "!!"
+    else
+    echo "Please execute "build.sh" instead"
+    fi
+else
+make O=out $BUILD_ARGS PLATFORM_VERSION=12 ANDROID_MAJOR_VERSION=s LLVM=1 LLVM_IAS=1 ARCH=arm64 TARGET_SOC=s5e8825 CROSS_COMPILE="$KERNEL_HOME/../clang-r416183b/bin/aarch64-linux-gnu-" m33x_defconfig
+make O=out $BUILD_ARGS PLATFORM_VERSION=12 ANDROID_MAJOR_VERSION=s LLVM=1 LLVM_IAS=1 ARCH=arm64 TARGET_SOC=s5e8825 CROSS_COMPILE="$KERNEL_HOME/../clang-r416183b/bin/aarch64-linux-gnu-" -j$(nproc --all)
+fi
