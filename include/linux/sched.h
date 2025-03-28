b9682beaed0d0733003df52a7c06f9c70fa83f58
fluffball3 <fluffball3@proton.me>
2025-03-29 00:24:38 +0530

    Reapply "latency_nice patch"
    This reverts commit feacc0bbb0d1b8a7fe6a4e2b9b26bc32d33f2d53.

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1389,7 +1389,9 @@ struct task_struct {
 	ANDROID_KABI_USE(1, void *pf_io_worker);
 
 	ANDROID_KABI_RESERVE(2);
-	ANDROID_KABI_RESERVE(3);
+
+	ANDROID_KABI_USE(3, int latency_nice);
+
 #ifdef CONFIG_KSU_SUSFS
 	ANDROID_KABI_USE(4, u64 susfs_task_state);
 	ANDROID_KABI_USE(5, u64 susfs_last_fake_mnt_id);
--- init/init_task.c
+++ init/init_task.c
@@ -84,6 +84,7 @@ struct task_struct init_task
 	.prio		= MAX_PRIO - 20,
 	.static_prio	= MAX_PRIO - 20,
 	.normal_prio	= MAX_PRIO - 20,
+	.latency_nice   = 0,
 	.policy		= SCHED_NORMAL,
 	.cpus_ptr	= &init_task.cpus_mask,
 	.cpus_mask	= CPU_MASK_ALL,
