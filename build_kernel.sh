11ae7634fa4bb3aa717b98876a2d9897e2156f28
fluffball3 <fluffball3@proton.me>
2025-04-28 01:20:19 +0530

    update to clang-r522817

--- build_kernel.sh
+++ build_kernel.sh
@@ -3,12 +3,12 @@
 KERNEL_HOME="$(pwd)"
 BUILD_ARGS="LOCALVERSION=-Elite3XP-v3.0 KBUILD_BUILD_USER=fluffyball21 KBUILD_BUILD_HOST=Inudesu"
 export PATH="$(pwd)/../build-tools/path/linux-x86:$PATH"
-export PATH="$(pwd)/../clang-r450784/bin:$PATH"
-export CC="$(pwd)/../clang-r450784/bin/clang"
+export PATH="$(pwd)/../clang-r522817/bin:$PATH"
+export CC="$(pwd)/../clang-r522817/bin/clang"
 
 if [ ! -d "$KERNEL_HOME/../build-tools" ]; then
     echo "Please execute "build.sh" instead"
-    if [ ! -d "$KERNEL_HOME/../clang-r450784" ]; then
+    if [ ! -d "$KERNEL_HOME/../clang-r522817" ]; then
         if [ -d "$KERNEL_HOME/../build-tools" ]; then
         echo "!!"
         else
@@ -16,6 +16,6 @@ if [ ! -d "$KERNEL_HOME/../build-tools" ]; then
         fi
     fi
 else
-    make O=out $BUILD_ARGS CROSS_COMPILE="$KERNEL_HOME/../clang-r450784/bin/aarch64-linux-gnu-" PLATFORM_VERSION=12 ANDROID_MAJOR_VERSION=s LLVM=1 LLVM_IAS=1 ARCH=arm64 TARGET_SOC=s5e8825 m33x_defconfig
-    make O=out $BUILD_ARGS CROSS_COMPILE="$KERNEL_HOME/../clang-r450784/bin/aarch64-linux-gnu-" PLATFORM_VERSION=12 ANDROID_MAJOR_VERSION=s LLVM=1 LLVM_IAS=1 ARCH=arm64 TARGET_SOC=s5e8825 -j$(nproc --all)
+    make O=out $BUILD_ARGS CROSS_COMPILE="$KERNEL_HOME/../clang-r522817/bin/aarch64-linux-gnu-" PLATFORM_VERSION=12 ANDROID_MAJOR_VERSION=s LLVM=1 LLVM_IAS=1 ARCH=arm64 TARGET_SOC=s5e8825 m33x_defconfig
+    make O=out $BUILD_ARGS CROSS_COMPILE="$KERNEL_HOME/../clang-r522817/bin/aarch64-linux-gnu-" PLATFORM_VERSION=12 ANDROID_MAJOR_VERSION=s LLVM=1 LLVM_IAS=1 ARCH=arm64 TARGET_SOC=s5e8825 -j$(nproc --all)
 fi
