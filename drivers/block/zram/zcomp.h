9d1259263cc0d1835bec9d66d62b2e16aad6ee4f
fluffball3 <fluffball3@proton.me>
2025-07-31 23:07:57 +0530

    guard the rest of zram_lru_writeback code
    returns to aosp zram functionality

--- drivers/block/zram/zcomp.h
+++ drivers/block/zram/zcomp.h
@@ -13,7 +13,9 @@ struct zcomp_strm {
 	/* compression/decompression buffer */
 	void *buffer;
 	struct crypto_comp *tfm;
+#ifdef CONFIG_ZRAM_LRU_WRITEBACK
 	void *tmpbuf;
+#endif
 };
 
 /* dynamic per-device compression frontend */
