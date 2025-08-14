9476aa423280359e62ac08298f7689a996be9fd2
fluffball3 <fluffball3@proton.me>
2025-08-14 17:27:35 +0530

    Revert "import wireless/scsc from A266MUBU1AYB8"
    This reverts commit 6b8b8761d5b61b823b715330259d99be39108290.
    
    drains battery faster....

--- drivers/net/wireless/scsc/reg_info.c
+++ drivers/net/wireless/scsc/reg_info.c
@@ -1,6 +1,6 @@
 /******************************************************************************
  *
- * Copyright (c) 2012 - 2024 Samsung Electronics Co., Ltd. All rights reserved
+ * Copyright (c) 2012 - 2021 Samsung Electronics Co., Ltd. All rights reserved
  *
  *****************************************************************************/
 #include "dev.h"
@@ -12,9 +12,6 @@
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 4, 0))
 MODULE_IMPORT_NS(VFS_internal_I_am_really_a_filesystem_and_am_NOT_a_driver);
 #endif
-
-#define SLSI_REG_INFO_BUILD_REG_DOM_VER(major, minor, minor2) \
-		((major & 0xFF) << 16) | ((minor & 0xFF) << 8) | (minor2 & 0xFF)
 
 void slsi_regd_init(struct slsi_dev *sdev)
 {
@@ -83,11 +80,7 @@ int slsi_read_regulatory(struct slsi_dev *sdev)
 	const struct firmware *firm;
 
 	if (sdev->regdb.regdb_state == SLSI_REG_DB_SET) {
-		SLSI_INFO(sdev, "DB Ver:%d.%d.%d, Num Countries:%d\n", sdev->regdb.db_major_version,
-			  sdev->regdb.db_minor_version, sdev->regdb.db_2nd_minor_version, sdev->regdb.num_countries);
-		sdev->reg_dom_version = SLSI_REG_INFO_BUILD_REG_DOM_VER(sdev->regdb.db_major_version,
-									sdev->regdb.db_minor_version,
-									sdev->regdb.db_2nd_minor_version);
+		SLSI_INFO(sdev, "Regulatory is already set\n");
 		return 0;
 	}
 
@@ -102,12 +95,6 @@ int slsi_read_regulatory(struct slsi_dev *sdev)
 		SLSI_INFO(sdev, "Failed to read script version\n");
 		goto exit;
 	}
-
-	if (script_version > 3) {
-		SLSI_INFO(sdev, "DB unknown script version:%d. Abort DB file read\n", script_version);
-		goto exit;
-	}
-
 	if (firmware_read(firm, &sdev->regdb.db_major_version, sizeof(uint32_t), &offset) < 0) {
 		SLSI_INFO(sdev, "Failed to read regdb major version %u\n", sdev->regdb.db_major_version);
 		goto exit;
@@ -115,15 +102,6 @@ int slsi_read_regulatory(struct slsi_dev *sdev)
 	if (firmware_read(firm, &sdev->regdb.db_minor_version, sizeof(uint32_t), &offset) < 0) {
 		SLSI_INFO(sdev, "Failed to read regdb minor version %u\n", sdev->regdb.db_minor_version);
 		goto exit;
-	}
-
-	/*2nd minor number is introduced from script version 3 onwards.*/
-	sdev->regdb.db_2nd_minor_version = 0;
-	if (script_version >= 3) {
-		if (firmware_read(firm, &sdev->regdb.db_2nd_minor_version, sizeof(uint32_t), &offset) < 0) {
-			SLSI_INFO(sdev, "Failed to read regdb 2nd minor version %u\n", sdev->regdb.db_minor_version);
-			goto exit;
-		}
 	}
 	if (firmware_read(firm, &num_freqbands, sizeof(uint32_t), &offset) < 0) {
 		SLSI_INFO(sdev, "Failed to read Number of Frequency bands %u\n", num_freqbands);
@@ -196,9 +174,8 @@ int slsi_read_regulatory(struct slsi_dev *sdev)
 		SLSI_ERR(sdev, "Failed to read regdb number of countries\n");
 		goto exit_rules_collection;
 	}
-
-	SLSI_INFO(sdev, "DB Ver:%d.%d.%d, Script Ver:%d, Num Countries:%d\n", sdev->regdb.db_major_version,
-		  sdev->regdb.db_minor_version, sdev->regdb.db_2nd_minor_version, script_version,
+	SLSI_INFO(sdev, "Regulatory Version: %d.%d , Script Version: %d , Number of Countries: %d\n",
+		  sdev->regdb.db_major_version, sdev->regdb.db_minor_version, script_version,
 		  sdev->regdb.num_countries);
 
 	sdev->regdb.country = kmalloc(sizeof(*sdev->regdb.country) * sdev->regdb.num_countries, GFP_KERNEL);
@@ -228,9 +205,6 @@ int slsi_read_regulatory(struct slsi_dev *sdev)
 
 	mx140_release_file(sdev->maxwell_core, firm);
 	sdev->regdb.regdb_state = SLSI_REG_DB_SET;
-	sdev->reg_dom_version = SLSI_REG_INFO_BUILD_REG_DOM_VER(sdev->regdb.db_major_version,
-								sdev->regdb.db_minor_version,
-								sdev->regdb.db_2nd_minor_version);
 	return 0;
 
 exit_country:
