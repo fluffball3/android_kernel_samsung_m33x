4bc3836e16529c9337e6c60fe9ded3e210fbaea1
fluffball3 <fluffball3@proton.me>
2025-11-09 20:52:07 +0530

    sched/ems: ego: apply 35% boost

--- kernel/sched/ems/ego.c
+++ kernel/sched/ems/ego.c
@@ -976,7 +976,7 @@ static int get_boost_pelt_util(int capacity, int util, int boost)
         }
         margin /= 100;
 #endif
-	return util + margin + (util + margin) * 30/100;   	//boost by 30% more
+	return util + margin + (util + margin) * 35/100;   	//boost by 35% more
 }
 
 static unsigned int ego_next_freq_shared(struct ego_cpu *egc, u64 time)
