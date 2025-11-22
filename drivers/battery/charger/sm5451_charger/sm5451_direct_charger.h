eeedb1f91b0970151ce939e1bc27284765ac7194
Flopster101 <nahuelgomez329@gmail.com>
2025-11-22 17:29:11 -0300

    drivers: Remove MIN and MAX definitions from several Samsung drivers
    Now provided by minmax.h, since [1].
    
    [1] aosp-mirror/kernel_common@6183c65
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- drivers/battery/charger/sm5451_charger/sm5451_direct_charger.h
+++ drivers/battery/charger/sm5451_charger/sm5451_direct_charger.h
@@ -32,8 +32,8 @@
 #define CC_ST_IBUS_OFFSET       100
 #define CV_ST_SUB_DC_OFF_IBUS   1000
 
-#define MAX(a, b)               ((a > b) ? (a):(b))
-#define MIN(a, b)               ((a < b) ? (a):(b))
+// #define MAX(a, b)               ((a > b) ? (a):(b))
+// #define MIN(a, b)               ((a < b) ? (a):(b))
 
 enum sm_dc_charging_loop {
 	LOOP_IBUSREG                = (0x1 << 7),
