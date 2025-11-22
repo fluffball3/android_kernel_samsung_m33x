a0e31b4ee44d755f2c0cffa03acccc1871786c57
fluffball3 <fluffball3@proton.me>
2025-11-22 12:23:28 +0530

    set page_cluster to 0

--- mm/swap.c
+++ mm/swap.c
@@ -1336,13 +1336,17 @@ EXPORT_SYMBOL(pagevec_lookup_range_nr_tag);
  */
 void __init swap_setup(void)
 {
+	/* set page_cluster to 0
 	unsigned long megs = totalram_pages() >> (20 - PAGE_SHIFT);
 
-	/* Use a smaller cluster for small-memory machines */
+	// Use a smaller cluster for small-memory machines
 	if (megs < 16)
 		page_cluster = 2;
 	else
 		page_cluster = 3;
+	*/
+	page_cluster = 0;
+
 	/*
 	 * Right now other parts of the system means that we
 	 * _really_ don't want to cluster much more
