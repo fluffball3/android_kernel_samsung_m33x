4f021273583d9828d731f4c45963bfe406809ca3
fluffball3 <fluffball3@proton.me>
2025-12-01 20:30:14 +0530

    Revert "sched/ems: don't boost if util is low"
    This reverts commit 761fbf63b170f38e20224a328976a78f4a515790.

--- kernel/sched/ems/ego.c
+++ kernel/sched/ems/ego.c
@@ -976,9 +976,7 @@ static int get_boost_pelt_util(int capacity, int util, int boost)
         }
         margin /= 100;
 #endif
-	if (util + margin < 125)
-		return util + margin;
-	return (util + margin) * 135/100; 	//boost by 35%
+	return util + margin + (util + margin) * 35/100;   	//boost by 35% more
 }
 
 static unsigned int ego_next_freq_shared(struct ego_cpu *egc, u64 time)
