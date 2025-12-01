bc3567c5756ca845125b80922c5ce53e2ce189b9
fluffball3 <fluffball3@proton.me>
2025-12-01 20:37:54 +0530

    sched/ems: ego: implement custom util boost curve
    lowers util when less than 135 and boosts when higher. based on the function (x + (x - (135 + (x - 135) / 2.2))).
    
    Signed-off-by: Shon Sebastian <fluffball3@proton.me>

--- kernel/sched/ems/ego.c
+++ kernel/sched/ems/ego.c
@@ -976,7 +976,7 @@ static int get_boost_pelt_util(int capacity, int util, int boost)
         }
         margin /= 100;
 #endif
-	return util + margin;
+	return (34 * (util + margin) - 1620) / 22;
 }
 
 static unsigned int ego_next_freq_shared(struct ego_cpu *egc, u64 time)
