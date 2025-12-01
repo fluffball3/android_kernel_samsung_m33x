f89c6753b05008d6b46200ce61763c300da7cc0c
fluffball3 <fluffball3@proton.me>
2025-12-01 13:12:55 +0530

    Revert "sched/ems: ego: increase util needed for boost"
    This reverts commit 626a0f8a80731b6ac83186fcbfcbc27a624b8d0a.

--- kernel/sched/ems/ego.c
+++ kernel/sched/ems/ego.c
@@ -976,7 +976,7 @@ static int get_boost_pelt_util(int capacity, int util, int boost)
         }
         margin /= 100;
 #endif
-	if (util + margin < 135)
+	if (util + margin < 125)
 		return util + margin;
 	return (util + margin) * 135/100; 	//boost by 35%
 }
