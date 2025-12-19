38bd0c7dcc565b7f8f91fabd719a616f2ab7e66e
fluffball3 <fluffball3@proton.me>
2025-12-20 01:27:46 +0530

    [SQUASH] revert ANDROID_SIMPLE_LMK patches (by kerneltoast)
    android version above 10 has userspace lmkd which conflicts with kernel lmk. revert for now.

--- kernel/fork.c
+++ kernel/fork.c
@@ -97,7 +97,6 @@
 #include <linux/io_uring.h>
 #include <linux/cpufreq_times.h>
 #include <linux/task_integrity.h>
-#include <linux/simple_lmk.h>
 
 #include <asm/pgalloc.h>
 #include <linux/uaccess.h>
@@ -1164,7 +1163,6 @@ static inline void __mmput(struct mm_struct *mm)
 	ksm_exit(mm);
 	khugepaged_exit(mm); /* must run before exit_mmap */
 	exit_mmap(mm);
-	simple_lmk_mm_freed(mm);
 	mm_put_huge_zero_page(mm);
 	set_mm_exe_file(mm, NULL);
 	if (!list_empty(&mm->mmlist)) {
