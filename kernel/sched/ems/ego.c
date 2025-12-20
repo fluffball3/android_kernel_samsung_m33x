5d55e110b1dc5453f5762a94ce28f49912d9c757
fluffball3 <fluffball3@proton.me>
2025-12-21 01:26:19 +0530

    Revert "sched/ems: ego: use boosting util curve"
    This reverts commit 4b18bc50990f64583d9fe3ce15c3e306730536a5.
    
    doesnt work properly at all..... even tho boosting alone is ok, reducing the util is problematic so reverting....

--- kernel/sched/ems/ego.c
+++ kernel/sched/ems/ego.c
@@ -976,8 +976,7 @@ static int get_boost_pelt_util(int capacity, int util, int boost)
         }
         margin /= 100;
 #endif
-	margin = util + margin;
-	return margin + (margin - 252) / 10;
+	return util + margin;
 }
 
 static unsigned int ego_next_freq_shared(struct ego_cpu *egc, u64 time)
