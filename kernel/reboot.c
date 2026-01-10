3679d985506a1c37b4961a1cd2c422c7a8f57e57
fluffball3 <fluffball3@proton.me>
2026-01-11 01:25:26 +0530

    Revert "scope_min_manual_hooks_v1.6 patch"
    This reverts commit 89a190b7ef206ec522a27787210ebe950b485ea9.
    
    no need for these when susfs is present

--- kernel/reboot.c
+++ kernel/reboot.c
@@ -322,11 +322,6 @@ extern int ksu_handle_sys_reboot(int magic1, int magic2, unsigned int cmd, void 
  *
  * reboot doesn't sync: do that yourself before calling this.
  */
-
-#ifdef CONFIG_KSU
-extern int ksu_handle_sys_reboot(int magic1, int magic2, unsigned int cmd, void __user **arg);
-#endif
-
 SYSCALL_DEFINE4(reboot, int, magic1, int, magic2, unsigned int, cmd,
 		void __user *, arg)
 {
@@ -341,10 +336,6 @@ SYSCALL_DEFINE4(reboot, int, magic1, int, magic2, unsigned int, cmd,
 	}
 	return ret;
 orig_flow:
-#endif
-
-#ifdef CONFIG_KSU 
-	ksu_handle_sys_reboot(magic1, magic2, cmd, &arg);
 #endif
 
 	/* We only trust the superuser with rebooting the system. */
