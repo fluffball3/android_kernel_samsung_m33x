7ca4a5630d8447e5d92d0a3e6130ff72a92af9c3
fluffball3 <fluffball3@proton.me>
2026-02-06 01:19:09 +0530

    Revert "block: set zram default compressor to lz4"
    This reverts commit 0ddc7c79abb5d834c0b38f97b8b7e4554304383d.
    
    its smoother and more battery efficiemt, except having less headroom overall but it shud be fine.

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -59,7 +59,7 @@ static DEFINE_IDR(zram_index_idr);
 static DEFINE_MUTEX(zram_index_mutex);
 
 static int zram_major;
-static const char *default_compressor = "lz4";
+static const char *default_compressor = "lzo-rle";
 
 static bool is_lzorle;
 #ifdef CONFIG_ZRAM_LRU_WRITEBACK
