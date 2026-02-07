c1ade598819c0d093c4416399caa92eb770af733
fluffball3 <fluffball3@proton.me>
2026-02-07 20:55:34 +0530

    sched/ems: ego: increase boost aggressiveness
    Signed-off-by: fluffball3 <fluffball3@proton.me>

--- kernel/sched/ems/ego.c
+++ kernel/sched/ems/ego.c
@@ -1009,7 +1009,7 @@ static unsigned int ego_next_freq_shared(struct ego_cpu *egc, u64 time)
 		util = max(util, egc->util);
 	}
 
-	cpu_boosted_util = util + egc->prev_util / 5;
+	cpu_boosted_util = util + egc->prev_util / 4;
 	egc->prev_util = util;
 
 	return get_next_freq(egp, cpu_boosted_util, max_cap);
