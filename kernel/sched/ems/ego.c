b952072f5b7ad6983392c84dd8e443a79fdde1b6
fluffball3 <fluffball3@proton.me>
2026-03-27 02:27:33 +0530

    sched/ems: decrease boosting drastically
    its too much boosting...  8 keeps a balance between 5 and 10.
    
    Signed-off-by: fluffball3 <fluffball3@proton.me>

--- kernel/sched/ems/ego.c
+++ kernel/sched/ems/ego.c
@@ -890,7 +890,7 @@ static unsigned int ego_next_freq_shared(struct ego_cpu *egc, u64 time)
 		ego_get_util(egc, boost);
 		egc->pelt_util = egc->util;
 
-		cpu_boosted_util = egc->util + ((egc->prev_util * egc->is_utilized) / 5);
+		cpu_boosted_util = egc->util + ((egc->prev_util * egc->is_utilized) / 8);
 		egc->boosted_util = cpu_boosted_util;
 		egc->prev_util = egc->util;
 
