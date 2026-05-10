c56cde421f7785fefd39c4b9170df09a049353b5
fluffball3 <fluffball3@proton.me>
2026-05-10 21:13:19 +0530

    update to clang 19

--- kernel_build/build.sh
+++ kernel_build/build.sh
@@ -58,18 +58,18 @@ kfinish
 DIR="$(readlink -f .)"
 PARENT_DIR="$(readlink -f ${DIR}/..)"
 
-export CROSS_COMPILE="$PARENT_DIR/clang-r416183b/bin/aarch64-linux-gnu-"
-export CC="$PARENT_DIR/clang-r416183b/bin/clang"
+export CROSS_COMPILE="$PARENT_DIR/clang-r530567/bin/aarch64-linux-gnu-"
+export CC="$PARENT_DIR/clang-r530567/bin/clang"
 
 export PLATFORM_VERSION=12
 export ANDROID_MAJOR_VERSION=s
-export PATH="$PARENT_DIR/build-tools/path/linux-x86:$PARENT_DIR/clang-r416183b/bin:$PATH"
+export PATH="$PARENT_DIR/build-tools/path/linux-x86:$PARENT_DIR/clang-r530567/bin:$PATH"
 export TARGET_SOC=s5e8825
 export LLVM=1 LLVM_IAS=1
 export ARCH=arm64
 
-if [ ! -d "$PARENT_DIR/clang-r416183b" ]; then
-    git clone https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r416183b "$PARENT_DIR/clang-r416183b" --depth=1
+if [ ! -d "$PARENT_DIR/clang-r530567" ]; then
+    git clone https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r530567 "$PARENT_DIR/clang-r530567" --depth=1
 fi
 
 if [ ! -d "$PARENT_DIR/build-tools" ]; then
