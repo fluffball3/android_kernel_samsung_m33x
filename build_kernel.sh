03c5f023f35f987af0f07715fb1922cd77dd286c
fluffball3 <fluffball3@proton.me>
2026-05-11 20:04:21 +0530

    add build script
    referenced from Gabriel260BR's script. mostly for testing.

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-export PATH=$(pwd)/toolchain/clang/host/linux-x86/clang-r416183b/bin:$PATH
-export PATH=$(pwd)/toolchain/build/build-tools/path/linux-x86:$(pwd)/toolchain/prebuilts/gas/linux-x86:$PATH
+KERNEL_HOME="$(pwd)"
+export PATH="$(pwd)/../build-tools/path/linux-x86:$PATH"
+export PATH="$(pwd)/../clang-r530567/bin:$PATH"
+export CC="$(pwd)/../clang-r530567/bin/clang"
 
-make PLATFORM_VERSION=12 ANDROID_MAJOR_VERSION=s LLVM=1 LLVM_IAS=1 ARCH=arm64 TARGET_SOC=s5e8825 CROSS_COMPILE=$(pwd)/toolchain/clang/host/linux-x86/clang-r416183b/bin/aarch64-linux-gnu- s5e8825-m33xins_defconfig
-make PLATFORM_VERSION=12 ANDROID_MAJOR_VERSION=s LLVM=1 LLVM_IAS=1 ARCH=arm64 TARGET_SOC=s5e8825 CROSS_COMPILE=$(pwd)/toolchain/clang/host/linux-x86/clang-r416183b/bin/aarch64-linux-gnu- -j32
+if [ ! -d "$KERNEL_HOME/../build-tools" ]; then
+    echo "Please execute "build.sh" instead"
+    if [ ! -d "$KERNEL_HOME/../clang-r530567" ]; then
+        if [ -d "$KERNEL_HOME/../build-tools" ]; then
+        echo "!!"
+        else
+        echo "Please execute "build.sh" instead"
+        fi
+    fi
+else
+    make O=out CROSS_COMPILE="$KERNEL_HOME/../clang-r530567/bin/aarch64-linux-gnu-" PLATFORM_VERSION=12 ANDROID_MAJOR_VERSION=s LLVM=1 LLVM_IAS=1 ARCH=arm64 TARGET_SOC=s5e8825 m33x_defconfig menuconfig
+    #make O=out CROSS_COMPILE="$KERNEL_HOME/../clang-r530567/bin/aarch64-linux-gnu-" PLATFORM_VERSION=12 ANDROID_MAJOR_VERSION=s LLVM=1 LLVM_IAS=1 ARCH=arm64 TARGET_SOC=s5e8825 -j$(nproc --all)
+fi
